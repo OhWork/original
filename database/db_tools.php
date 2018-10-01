f34c618e5fded1a58c07c3b13731df623f5a4740
choatchaw <choatchaw@gmail.com>
2018-10-01 11:26:09 +0700

    งง

--- database/db_tools.php
+++ database/db_tools.php
@@ -5,7 +5,8 @@
         private  $pass = "";
         private  $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
         protected $con;
- 
+        public $sql="";
+        public $stm="";
         public function openConnection(){
                try
                  {
@@ -18,10 +19,40 @@
                  }
  
         }
- 
         public function closeConnection(){
             $this->con = null;
         }
- 
+        
+        function createStement($textsql){
+            $this->sql=$textsql;
+            $this->stm = $this->con->prepare($this->sql);
+            }
+        function Stement(){
+            $this->stm = $this->con->prepare($this->sql);
+            }
+        function runStmSql($param=array()){
+            $this->stm->execute($param);
+        }
+        
+        function moveNext_getRow($mode ='num'){
+            $arrmode = array("num"=>PDO::FETCH_NUM,"assoc"=>PDO::FETCH_ASSOC);
+            return $this->stm->fetch($arrmode[$mode]);
+        }
+        
+        function closeStm(){
+            $this->stm->closeCursor();
+        }
+		public function findAll($table){
+				$this->sql = 'SELECT * FROM '.$table;
+				return $this;
+			}
+        function conditions($table,$condition){
+			$this->sql = "SELECT * FROM $table WHERE $condition";
+			return $this;
+		}
+		function findByPK($table,$column,$value){
+			$this->sql = "SELECT * FROM $table WHERE $column = $value";
+			return $this;
+		}
 }
 ?>
--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
     include 'database/db_tools.php';
-    
+    //ตัวอย่าง
     $db = new db_tools();
-    $con = $db->openConnection();    
-    if($con){
-        echo "get it";
-    }else{
-        echo "not";
-    }
+    $db->openConnection();    
+    // $db->createStement("select * from zoo where zoo_enable = :sts");
+//      $db->conditions("zoo","zoo_type = :sts")->Stement();
+//     $db->findByPK("zoo","zoo_id",":sts")->Stement();
+    $db->findAll("zoo")->Stement();
+     $db->runStmSql(array());
+    while($cols = $db->moveNext_getRow()){
+        echo $cols[1],"<br>";
+        // echo $cols['zoo_name'],"<br>";
+}    
+    echo $db->closeConnection();
 ?>
