34ceeffe4d1c801b90968aa33a94ced1ed10caab
Firore <takbasic722@gmail.com>
2018-10-09 22:45:48 +0700

    งง กว่า

--- index.php
+++ index.php
@@ -1,4 +1,44 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>Original</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/dashboard.css" >
+        <link rel="stylesheet" href="css/jquery-ui.css">
+        <link rel="stylesheet" href="css/main.css">
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+<body>
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark col-md-12">
+		<a class="navbar-brand brandedit" href="#"><h4>Original</h4></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarCollapse">
+				<ul class="navbar-nav mr-auto"></ul>
+				<div class="form-inline">
+					<a class="btn lgn" style="padding-top:10px;padding-bottom:10px;padding-right:0;border-right:1px solid #1DE9B6;border-radius:0px;" href="http://www.zoothailand.org/ewt_news.php?nid=246">
+					<div style="padding-right:15px;">About us</div></a>
+				</div>
+				<div class="form-inline">
+					<a class="btn lgn" href="login.php" title="Log-in">
+						<div class="ml-1" style="float:left;">Log-in</div>
+					</a>
+				</div>
+			</div>
+	</nav>
+	<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 showindex" >
+		<div class="row">
+			<?php include 'menu_main.php'; ?>
+		</div>
+	</div>
+			<?php
     include 'database/db_tools.php';
     //ตัวอย่าง
     $db = new db_tools();
@@ -14,3 +54,21 @@
 }    
     echo $db->closeConnection();
 ?>
+<script>
+        // passes on every "a" tag
+        $("a").each(function() {
+// 	       	console.log(this.href);
+            // checks if its the same on the address bar
+            if (url == (this.href)) {
+// 	            console.log(1234);
+                $(this).parents(0).addClass("show");
+                $(this).addClass("bcmn");
+				$(this).children().addClass('bcnm');
+                //for making parent of submenu active
+               //$(this).closest("li").parent().parent().addClass("active");
+               $(this).parents(0).attr("aria-expanded", true);
+//                console.log($(this).parents());
+            }
+        });
+</script>
+</html>
